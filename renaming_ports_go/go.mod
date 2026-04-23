b26d1b1bc138e6607ea360ebb2407d81944106c8
Randolph Harrison <randolph.s.harrison@gmail.com>
2026-04-23 14:52:46 -0400

    oid test

--- renaming_ports_go/go.mod
+++ renaming_ports_go/go.mod
@@ -1,3 +1,7 @@
 module github.com/Randolph-Harrison/networking_scripts/renaming_ports_go
 
 go 1.26.2
+
+require github.com/gosnmp/gosnmp v1.43.2
+
+require github.com/joho/godotenv v1.5.1 // indirect
--- renaming_ports_go/renaming_ports.go
+++ renaming_ports_go/renaming_ports.go
@@ -1,7 +1,62 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"log"
+	"os"
+	"time"
+
+	"github.com/gosnmp/gosnmp"
+	"github.com/joho/godotenv"
+)
+
+const (
+	SysDescription = "1.3.6.1.2.1.1.1.0"
+)
 
 func main() {
-	fmt.Println("Hello, World!")
+	err := godotenv.Load()
+	if err != nil {
+		log.Fatal("Error loading .env file")
+	}
+
+	// reader := bufio.NewReader(os.Stdin)
+	// fmt.Print("Enter your full name: ")
+	// input, _ := reader.ReadString('\n')
+
+	// fmt.Println(input)
+	g := &gosnmp.GoSNMP{
+		Target:  os.Getenv("SWITCH_IP"),
+		Port:    161,
+		Version: gosnmp.Version3,
+		Timeout: time.Duration(5),
+
+		MsgFlags:      gosnmp.AuthPriv,
+		SecurityModel: gosnmp.UserSecurityModel,
+		SecurityParameters: &gosnmp.UsmSecurityParameters{
+			UserName:                 os.Getenv("SNMP_USER"),
+			AuthenticationProtocol:   gosnmp.MD5,
+			AuthenticationPassphrase: os.Getenv("SNMP_PASS"),
+			PrivacyProtocol:          gosnmp.AES,
+			PrivacyPassphrase:        os.Getenv("SNMP_PRIV"),
+		},
+	}
+	err = g.Connect()
+	if err != nil {
+		log.Fatalf("ERROR: failed to connect: %v", err)
+	}
+	defer g.Conn.Close()
+
+	oids := []string{SysDescription}
+	result, err := g.Get(oids)
+	if err != nil {
+		log.Fatal("ERROR: failed getting SNMP: %v", err)
+	}
+
+	for i, variable := range result.Variables {
+		fmt.Printf("%d: oid: %s ", i, variable.Name)
+
+		bytes := variable.Value.([]byte)
+		fmt.Printf("string: %s\n", string(bytes))
+	}
 }
